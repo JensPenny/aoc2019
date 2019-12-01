35f5aaf4fd307e3bbf5c34312f8e919c06ea0f50
Jens Penny <el.tutter@gmail.com>
2019-12-01 22:28:11 +0100

    beautify used

--- src/tools/filereader.ts
+++ src/tools/filereader.ts
@@ -1,23 +1,20 @@
 import * as fs from 'fs';
 
 export class FileReader {
-    read(location:string) : Promise<any> {
+    read(location: string): Promise < any > {
         return new Promise((resolve, reject) => {
             fs.readFile(location, 'utf8', (err, data) => {
-                if (err){
+                if (err) {
                     reject(err)
                 } else {
                     resolve(data)
                 }
-            });  
+            });
         });
     }
 
-    asStringList(data:string) : Array<String> {
+    asStringList(data: string): Array < String > {
         return data.split('\n')
     }
-    
-}
 
-//let file = new FileReader()
-//file.read("./assets/test.txt")
+}
