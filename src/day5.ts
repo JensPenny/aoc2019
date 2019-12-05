a1dc92cf61ebf44e8689f1a2b888259519347506
Jens Penny <el.tutter@gmail.com>
2019-12-05 22:26:43 +0100

    day 1 and 2

--- src/day5.ts
+++ src/day5.ts
@@ -20,17 +20,28 @@ class OpCode {
 
 let reader = new FileReader
 
+//testProgram('1101,100,-1,4,0', [])
 //testProgram('3,0,4,0,99', [110])
 //testProgram('1002,4,3,4,33', [])
+//testProgram('3,21,1008,21,8,20,1005,20,22,107,8,21,20,1006,20,31,1106,0,36,98,0,0,1002,21,125,20,4,20,1105,1,46,104,999,1105,1,46,1101,1000,1,20,4,20,1105,1,46,98,99',[9])
 
 reader.read("./assets/day5")
     .then(data => day5A(data))
     .catch(err => console.error(err))
 
+reader.read("./assets/day5")
+    .then(data => day5B(data))
+    .catch(err => console.error(err))
 
 function day5A(data: string) {
     let codes = reader.asStringList(data, ',').map(s => Number(s));
     let result = runProgram(codes, [1])
+    console.log(result)
+}
+
+function day5B(data: string) {
+    let codes = reader.asStringList(data, ',').map(s => Number(s));
+    let result = runProgram(codes, [5])
     console.log(result)
 }
 
@@ -47,6 +58,7 @@ function runProgram(codes: number[], inputStream: number[]): number[] {
     while (currentIndex < codes.length) {
         let code = codes[currentIndex];
         let opCode = new OpCode(code);
+        console.log('coding ' + opCode.code)
         switch (opCode.code) {
             case 99: {
                 currentIndex = codes.length; //stop op
@@ -87,11 +99,73 @@ function runProgram(codes: number[], inputStream: number[]): number[] {
                 break;
             }
             case 4: {
-                let outputValue = codes[currentIndex + 1] //Write param
-                outputStream.push(outputValue)
+                let param1 = codes[currentIndex + 1] //Write param - hurr durr opgave
+                let val1 = opCode.p1Pos ? codes[param1] : param1
+
+                outputStream.push(val1)
+                console.log('outputting ' + val1)
                 currentIndex += 2
                 break;
             }
+            case 5: {
+                let param1 = codes[currentIndex + 1]
+                let param2 = codes[currentIndex + 2]
+                let val1 = opCode.p1Pos ? codes[param1] : param1
+                let val2 = opCode.p2Pos ? codes[param2] : param2
+
+                if (val1 != 0) {
+                    currentIndex = val2
+                } else {
+                    currentIndex += 3
+                }
+                break
+            }
+            case 6: {
+                let param1 = codes[currentIndex + 1]
+                let param2 = codes[currentIndex + 2]
+                let val1 = opCode.p1Pos ? codes[param1] : param1
+                let val2 = opCode.p2Pos ? codes[param2] : param2
+
+                if (val1 == 0) {
+                    currentIndex = val2
+                } else {
+                    currentIndex += 3
+                }
+                break
+            }
+            case 7: {
+                let param1 = codes[currentIndex + 1]
+                let param2 = codes[currentIndex + 2]
+                let param3 = codes[currentIndex + 3]
+                let val1 = opCode.p1Pos ? codes[param1] : param1
+                let val2 = opCode.p2Pos ? codes[param2] : param2
+                let pos = param3 //Write param
+
+                if (val1 < val2) {
+                    codes[pos] = 1
+                } else {
+                    codes[pos] = 0
+                }
+                currentIndex += 4;
+                break
+            }
+            case 8: {
+                let param1 = codes[currentIndex + 1]
+                let param2 = codes[currentIndex + 2]
+                let param3 = codes[currentIndex + 3]
+                let val1 = opCode.p1Pos ? codes[param1] : param1
+                let val2 = opCode.p2Pos ? codes[param2] : param2
+                let pos = param3 //Write param
+
+                if (val1 == val2) {
+                    codes[pos] = 1
+                } else {
+                    codes[pos] = 0
+                }
+                currentIndex += 4;
+                break
+            }
+        
             default:
                 console.log('Unknown opcode ' + opCode.code);
                 currentIndex += 1;
