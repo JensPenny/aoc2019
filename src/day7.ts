a67bf42ba27f884f05febe2395fdf384d09135c6
Jens Penny <el.tutter@gmail.com>
2019-12-10 00:47:40 +0100

    fixes, still an error in the intcode - PC I think, but earlier results work :/

--- src/day7.ts
+++ src/day7.ts
@@ -23,27 +23,31 @@ class Acs {
 var permutation = require('array-permutation'); //whatever dude
 //var iter = perm([1, 2, 3, 4]);
 
-day7('3,15,3,16,1002,16,10,16,1,16,15,15,4,15,99,0,0', [[4,3,2,1,0]])
-day7('3,31,3,32,1002,32,10,32,1001,31,-2,31,1007,31,0,33,1002,33,7,33,1,33,31,31,1,32,31,31,4,31,99,0,0,0', [[1,0,4,3,2]])
+day7A('3,15,3,16,1002,16,10,16,1,16,15,15,4,15,99,0,0', [
+    [4, 3, 2, 1, 0]
+])
+day7A('3,31,3,32,1002,32,10,32,1001,31,-2,31,1007,31,0,33,1002,33,7,33,1,33,31,31,1,32,31,31,4,31,99,0,0,0', [
+    [1, 0, 4, 3, 2]
+])
 
 let reader = new FileReader
 reader.read("./assets/day7")
     .then(data => day7A(data, permutation([0, 1, 2, 3, 4])))
     .catch(err => console.error(err))
 
-function runAcs(codes: number[], input: number[], program: Acs): Promise<number[]> {
+function runAcs(codes: number[], input: number[], program: Acs): Promise < number[] > {
     return new Promise((resolve, reject) => {
         let output = program.run(codes, input)
         resolve(output)
     })
 }
 
-function day7(input : string, permutations: any) {    
+function day7A(input: string, permutations: any) {
     let codes = input.split(',').map(s => Number(s))
 
     let program = new IntCoder(true)
     let results = []
-    for (let [a,b,c,d,e] of permutations) {
+    for (let [a, b, c, d, e] of permutations) {
         let aout = program.runProgram(codes, [a, 0])
         let bout = program.runProgram(codes, [b, ...aout])
         let cout = program.runProgram(codes, [c, ...bout])
@@ -60,9 +64,13 @@ function day7(input : string, permutations: any) {
             */
     }
 
-    results.map(res => res[0]).reduce((max, current) => {
-        if (current > max){
-            return current
-        }
-        return max
+    let max = results
+        .map(res => res[0])
+        .reduce((max, current) => {
+            if (current > max) {
+                return current
+            }
+            return max
+        })
+    console.log('max = ' + max)
 }
