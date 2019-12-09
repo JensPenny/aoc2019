ee32ea8c176d2e32f8eb15e1d7cae1bfd34994c5
Jens Penny <el.tutter@gmail.com>
2019-12-10 00:44:56 +0100

    still finished the assignment, debugging tomorrow

--- src/day7.ts
+++ src/day7.ts
@@ -26,7 +26,10 @@ var permutation = require('array-permutation'); //whatever dude
 day7('3,15,3,16,1002,16,10,16,1,16,15,15,4,15,99,0,0', [[4,3,2,1,0]])
 day7('3,31,3,32,1002,32,10,32,1001,31,-2,31,1007,31,0,33,1002,33,7,33,1,33,31,31,1,32,31,31,4,31,99,0,0,0', [[1,0,4,3,2]])
 
-//day7('input', permutation([0, 1, 2, 3, 4]))
+let reader = new FileReader
+reader.read("./assets/day7")
+    .then(data => day7A(data, permutation([0, 1, 2, 3, 4])))
+    .catch(err => console.error(err))
 
 function runAcs(codes: number[], input: number[], program: Acs): Promise<number[]> {
     return new Promise((resolve, reject) => {
@@ -39,13 +42,15 @@ function day7(input : string, permutations: any) {
     let codes = input.split(',').map(s => Number(s))
 
     let program = new IntCoder(true)
+    let results = []
     for (let [a,b,c,d,e] of permutations) {
         let aout = program.runProgram(codes, [a, 0])
-        let bout = program.runProgram(codes, [b, aout[0]])
-        let cout = program.runProgram(codes, [c, bout[0]])
-        let dout = program.runProgram(codes, [d, cout[0]])
-        let eout = program.runProgram(codes, [e, dout[0]])
+        let bout = program.runProgram(codes, [b, ...aout])
+        let cout = program.runProgram(codes, [c, ...bout])
+        let dout = program.runProgram(codes, [d, ...cout])
+        let eout = program.runProgram(codes, [e, ...dout])
         console.log(eout)
+        results.push(eout)
         /*runAcs(_.clone(codes), [0], new Acs(a))
             .then(data => runAcs(_.clone(codes), data, new Acs(b)))
             .then(data => runAcs(_.clone(codes), data, new Acs(c)))
@@ -54,4 +59,10 @@ function day7(input : string, permutations: any) {
             .then(data => console.log(data))
             */
     }
+
+    results.map(res => res[0]).reduce((max, current) => {
+        if (current > max){
+            return current
+        }
+        return max
 }
